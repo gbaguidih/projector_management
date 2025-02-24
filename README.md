226d883334a0d3869138a15916637bcd2b2772f1
gbaguidih <gbaguidihillary039@gmail.com>
2025-02-24 10:52:23 +0100

    Mise en place des différents dossiers et du server

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # projector_management
+
+Membre du groupe:
+1- Hillary : Etudiant 1
+2- Fidélia : Etudiant 2
+3- Bruno : Etudiant 3
+
+
+Démarage du server --->  node server 
+
---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");                      // Importation du server express
+const bodyParser = require('body-parser');               // module pour accéder à la requete  
+const db = require("./config/db");                       // Importation de la base de donnée
+const createTable = require("./config/createTable");
+
+const app = express();
+app.use(bodyParser.json());
+
+app.listen(3000, () => {
+    console.log("Serveur démarré (http://localhost:3000)!");
+  });
+
+
+// Fermerture de la base de données lors de l'arrêt de l'application
+process.on('SIGINT', () => {
+    createTable.closeDatabase();
+    process.exit();
+  });
